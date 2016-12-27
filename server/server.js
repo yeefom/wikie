aab5d42eab1c8db6abd7e6c5e1c282d0614666e6
Yifeng Wang <yifeng@yeefom.com>
2016-12-26 21:45:41 -0800

    Set up dev and prod environment

--- server/server.js
+++ server/server.js
@@ -4,12 +4,16 @@ const path = require('path');
 const apiRouter = require('./api/api');
 
 const app = express();
-const port = 8080;
+const port = 8000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.resolve(__dirname, '../client')));
+app.use(express.static(path.resolve(__dirname, '../dist')));
 
 app.use('/api', apiRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+});
 
 app.listen(port);
 console.log(`wikie started on port ${port}`);
---
+++ webpack.config.babel.js
@@ -0,0 +1,70 @@
+import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.join(__dirname, '/client/index.html'),
+  filename: 'index.html',
+  inject: 'body'
+});
+
+const isProduction = process.env.npm_lifecycle_event === 'build';
+
+const ProdPlugins = [
+  new webpack.DefinePlugin({
+    'process.env':{
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress:{
+      warnings: true
+    }
+  })
+];
+
+const DevPlugins = [
+  new webpack.HotModuleReplacementPlugin()
+];
+
+const plugins = isProduction ? ProdPlugins : DevPlugins;
+const sourceMap = isProduction ? 'cheap-module-source-map' : 'cheap-module-eval-source-map';
+
+const config = {
+  entry: [
+    path.join(__dirname, '/client/app.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'app_bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+    ...plugins
+  ],
+  devtool: sourceMap
+};
+
+if (!isProduction) {
+  config.devServer = {
+    contentBase: path.join(__dirname, '/dist'),
+    hot: true,
+    inline: true,
+    progress: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        secure: false
+      }
+    }
+  };
+}
+
+export default config;
