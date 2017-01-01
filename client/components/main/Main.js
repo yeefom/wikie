0aeef0fdac67dac53b48d25e75752ea337478303
Yifeng Wang <yifeng@yeefom.com>
2016-12-31 21:45:21 -0800

    Add basic styling

--- client/components/main/Main.js
+++ client/components/main/Main.js
@@ -1,9 +1,12 @@
 import React, {PropTypes} from 'react';
 import NavContainer from '../nav/NavContainer';
+import globalStyles from '../../styles/globalStyles.css';
+import {bodyContainer} from './styles.css';
 
 export default function Main(props) {
   return (
-    <div>
+    <div className={bodyContainer}>
+      <h1>Wikie</h1>
       <NavContainer/>
       {props.children}
     </div>
--- client/components/nav/Nav.js
+++ client/components/nav/Nav.js
@@ -1,7 +1,14 @@
 import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+import {navContainer} from './style.css';
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <div>
+      <ul className={navContainer}>
+        <li><Link to="/feed" activeClassName="active">Feed</Link></li>
+        <li><Link to="/bookmarks" activeClassName="active">Bookmarks</Link></li>
+      </ul>
+    </div>
   );
 }
